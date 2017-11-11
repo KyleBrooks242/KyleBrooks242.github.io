91f06758ffa2701ae0e2a062d974e00ea2e62564
Cody Pero <31071014+cpero@users.noreply.github.com>
2017-11-11 04:47:07 -0500

    Ajax boys (#5)
    * PREPPING FOR THE AJAX - RICK FLAIR TIME. 
    
    WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
    
    * pushing AjaxPost.py
    
    * Trying to fix 405

--- AjaxPost.py
+++ AjaxPost.py
@@ -4,6 +4,12 @@ import sys
 import json
 import cgi
 
+class HomeView(generic.TemplateView):
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
 fs = cgi.FieldStorage()
 
 sys.stdout.write("Content-Type: application/json")
