b4639c71c80b1c6022601ad4b7774bd863074197
KyleBrooks242 <brookska@g.cofc.edu>
2017-11-11 04:11:32 -0500

    ReadAndCreate pickle support for mapping values to days of week

--- ReadAndCreateDic.py
+++ ReadAndCreateDic.py
@@ -6,10 +6,15 @@ df.fillna(0, inplace=True)
 def createDictionary():
     dicPrimaryCause = {}
     dicCrashType = {}
+    dicDayValues = {'0':'Sunday','1':'Monday', '2':'Tuesday','3':'Wednesday',
+                    '4':'Thursday','5':'Friday','6':'Saturday'}
+    
+    
+    
     for index, row in df.iterrows():
         dicPrimaryCause[int(row['PRIMARY_CAUSE_CD'])]= row['PRIMARY_CAUSE']
         dicCrashType[int(row['CRSH_TYPE_CD'])]= row['CRASH_TYPE']
-    return dicPrimaryCause, dicCrashType
+    return dicPrimaryCause, dicCrashType, dicDayValues
 
 
 def getData():
