3d2aeedf33c9a3cb1d028ae428ffa032daca22ab
psg <psg@192.168.128.1>
2017-11-11 03:26:58 -0500

    Attempting to get map to work.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-google.maps.event.addDomListener(window, 'load', init);
-
 function init() {
-	//var mapCanvas = document.getElementById('map');
 	
 	var myLatLng = {lat: 35.919576, lng: -79.053618};
 	
@@ -27,25 +24,4 @@ function init() {
 	}
 	
 	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-	
-	var marker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		title: 'Predictive Auto Incident Map'
-	});
-	
-	var trafficLayer = new google.maps.TrafficLayer();
-	trafficLayer.setMap(map);
-	
-	var contentString = '<div id="content">'+
-	'<h1>Predictive Auto Incident Map</h1>'+
-	'</div>';
-	
-	var infowindow = new google.maps.InfoWindow({
-		content: contentString
-	});
-	
-	marker.addListener('click', function() {
-		infowindow.open(map, marker);
-	});
 }
