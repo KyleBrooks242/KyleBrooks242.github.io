88d9af9ac89f6c86992cf790a4d6443fae99f3eb
Cody Pero <cpero@marketech.us>
2017-11-11 06:09:24 -0500

    Trying a websocket

--- AjaxPost.py
+++ AjaxPost.py
@@ -1,23 +1,49 @@
-import os
-from flask import Flask,render_template, request,json
+#!/usr/bin/env python
 
-app = Flask(__name__)
+from tornado.options import options, define, parse_command_line
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.wsgi
+import tornado.websocket
+import json
 
-@app.route('/')
-def hello():
-    return 'Welcome to Python Flask!'
+define('port', type=int, default=8080)
 
-@app.route('/signUp')
-def signUp():
-    print("got in get")
-    return render_template('signUp.html')
+class HelloHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.write('Hello from tornado')
 
-@app.route('/signUpUser', methods=['GET'])
-def signUpUser():
-    print("got in post")
-    user =  request.form['username'];
-    password = request.form['password'];
-    return json.dumps({'status':'OK','user':user,'pass':password});
+class MyWebSocket(tornado.websocket.WebSocketHandler):
+  clients = []
 
-if __name__=="__main__":
-    app.run()
+  def open(self):
+    # clients must be accessed through class object!!!
+    MyWebSocket.clients.append(self)
+    print ("\nWebSocket opened")
+
+  def on_message(self, message):
+    print ("msg recevied"), message
+    msg = json.loads(message) # todo: safety?
+
+    # send other clients this message
+    for c in MyWebSocket.clients:
+      if c != self:
+        c.write_message(msg)
+
+  def on_close(self):
+    print ("WebSocket closed")
+    # clients must be accessed through class object!!!
+    MyWebSocket.clients.remove(self)
+
+def main():
+  tornado_app = tornado.web.Application([
+      ('/hello-tornado', HelloHandler),
+      ('/websocket', MyWebSocket),
+      ])
+  server = tornado.httpserver.HTTPServer(tornado_app)
+  server.listen(options.port)
+  tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+  main()
