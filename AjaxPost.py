061114eb7123db916d45aae0e71f450e6f5e3717
Cody Pero <31071014+cpero@users.noreply.github.com>
2017-11-11 05:04:37 -0500

    Ajax boys (#6)
    * PREPPING FOR THE AJAX - RICK FLAIR TIME. 
    
    WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
    
    * pushing AjaxPost.py
    
    * Trying to fix 405
    
    * Updated AJAX
    
    * Flask with AjaxPost and fixes to main.js

--- AjaxPost.py
+++ AjaxPost.py
@@ -1,35 +1,9 @@
 #!/usr/bin/env python
-
-import sys
-import json
-import cgi
-
-class HomeView(generic.TemplateView):
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        context = self.get_context_data(object=self.object)
-        return self.render_to_response(context)
-
-fs = cgi.FieldStorage()
-
-sys.stdout.write("Content-Type: application/json")
-
-sys.stdout.write("\n")
-sys.stdout.write("\n")
-
-
-result = {}
-result['success'] = True
-result['message'] = "The command Completed Successfully"
-result['keys'] = ",".join(fs.keys())
-
-d = {}
-for k in fs.keys():
-    d[k] = fs.getvalue(k)
-
-result['data'] = d
-
-sys.stdout.write(json.dumps(result,indent=1))
-sys.stdout.write("\n")
-
-sys.stdout.close()
+from flask import render_template
+     
+    
+@app.route('/predictAccident', methods=['POST'])
+def predictAccident():
+    val =  request.form['key'];
+    val2 = request.form['key2'];
+    return json.dumps({'status':'OK','key':val,'key2':val2});
--- js/main.js
+++ js/main.js
@@ -30,13 +30,14 @@ $('#testButton').click(function(){
     console.log("posting to AjaxPost.py")
 
     $.ajax({
-        url: "AjaxPost.py",
-        type: "post",
-        datatype:"json",
+        url: '/predictAccident',
         data: {'key':'value','key2':'value2'},
-        success: function(response){
-            alert(response.message);
-            alert(response.keys);
+        type: 'POST',
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
         }
     });
 });
